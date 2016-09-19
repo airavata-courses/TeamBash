9c7fff3e74013e7a77b9215937ecc140174a8c95
rraavi <raghuveer.raavi@gmail.com>
2016-09-19 18:20:50 -0400

    #13 made changes to routes

--- core/javascript/ForecastTrigger/routes/api.js
+++ core/javascript/ForecastTrigger/routes/api.js
@@ -4,15 +4,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/forecastTrigger', function(req, res, next) {
-    var number = Math.random(0,1);
-    if(number==0) {
+    var number = Math.random() > 0.5 ? 0 : 1;
+    if(number==1) {
         var resultForecast = {forecast:'YES' ,num:number};
     }
     else {
         var resultForecast = {forecast: 'NO' , num:number};
     }
-    var resultForecastJSON = JSON.stringify(resultForecast)
-    res.json(resultForecastJSON);
+    res.json(resultForecast);
 });
 
 module.exports = router;
