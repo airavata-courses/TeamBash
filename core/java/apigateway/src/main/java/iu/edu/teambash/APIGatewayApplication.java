4276eff986b067ebfbd0dbdcb96d9205f9e65b57
Janak Bhalla <janak1710@gmail.com>
2016-09-22 15:43:06 -0400

    Shifted User Authentication to Registry Service

--- core/java/apigateway/src/main/java/iu/edu/teambash/APIGatewayApplication.java
+++ core/java/apigateway/src/main/java/iu/edu/teambash/APIGatewayApplication.java
@@ -1,29 +1,12 @@
 package iu.edu.teambash;
 
 import io.dropwizard.Application;
-import io.dropwizard.auth.AuthDynamicFeature;
-import io.dropwizard.auth.AuthValueFactoryProvider;
-import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
-import io.dropwizard.db.DataSourceFactory;
-import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import iu.edu.teambash.auth.UserAuthenticator;
-import iu.edu.teambash.core.UsersEntity;
-import iu.edu.teambash.db.UserDao;
 import iu.edu.teambash.health.TemplateHealthCheck;
 import iu.edu.teambash.resources.DataIngestorResource;
-import iu.edu.teambash.resources.LoginResource;
 
 public class APIGatewayApplication extends Application<APIGatewayConfiguration> {
-
-    private final HibernateBundle<APIGatewayConfiguration> hibernateBundle =
-            new HibernateBundle<APIGatewayConfiguration>(UsersEntity.class) {
-                @Override
-                public DataSourceFactory getDataSourceFactory(APIGatewayConfiguration configuration) {
-                    return configuration.getDataSourceFactory();
-                }
-            };
 
     public static void main(final String[] args) throws Exception {
         new APIGatewayApplication().run(args);
@@ -36,26 +19,16 @@ public class APIGatewayApplication extends Application<APIGatewayConfiguration> 
 
     @Override
     public void initialize(final Bootstrap<APIGatewayConfiguration> bootstrap) {
-        bootstrap.addBundle(hibernateBundle);
     }
 
 
     @Override
     public void run(final APIGatewayConfiguration configuration,
                     final Environment environment) {
-        final UserDao dao = new UserDao(hibernateBundle.getSessionFactory());
-        final LoginResource resource = new LoginResource(dao
-        );
         final DataIngestorResource dataIngestorResource = new DataIngestorResource();
         final TemplateHealthCheck healthCheck =
                 new TemplateHealthCheck("hello");
         environment.healthChecks().register("template", healthCheck);
-        environment.jersey().register(new AuthDynamicFeature(new BasicCredentialAuthFilter.Builder<UsersEntity>()
-                .setAuthenticator(new UserAuthenticator())
-                .setRealm("User Authenticator")
-                .buildAuthFilter()));
-        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(UsersEntity.class));
-        environment.jersey().register(resource);
         environment.jersey().register(dataIngestorResource);
     }
 
