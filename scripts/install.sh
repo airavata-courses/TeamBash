67baa74d64c9a17de3d5825f5b1d3724d7665a42
Manikandan Murugesan <manikandan.m@live.co.uk>
2016-09-26 11:41:17 -0400

    Fixing path to stop overwriting of folders

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 echo 'Installing Registry' 
-cd "/home/ec2-user/core/java/registry"
-mvn -e clean install
-java -jar target/registry-1.0.0.jar server registry.yml
+cd "/home/ec2-user/registry"
+sudo mvn -e clean install
+nohup java -jar target/registry-1.0.0.jar server registry.yml &
