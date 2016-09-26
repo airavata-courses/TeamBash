7a64b06358212c38183efd20cbecb2f2a7238fea
rraavi <raghuveer.raavi@gmail.com>
2016-09-26 09:07:27 -0400

    #15 fixing scripts | sudo -> yum

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -7,5 +7,6 @@ sleep 20
 echo "installing node.js --version 4.x"
 
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo yum install -y nodejs 
+sudo yum install npm
 
