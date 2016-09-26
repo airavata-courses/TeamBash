9e9816db7c92c585a7b9cad6c3e6c277f4c0a0fe
sruthirameshv <srameshv@indiana.edu>
2016-09-26 16:14:32 -0400

    Created a new Directory

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+echo 'starting installation process' 
+cd '/home/ec2-user/DataIngestor/core/python/DataIngestor'
+
+pip install requests
+pip install Flask
+pip install boto
+export FLASK_APP=DataIngestor.py
+flask run --host=0.0.0.0 --port=65000 >> teambash.log 2>&1 &
