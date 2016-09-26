ae7f3627cfd7c89bfa2e685b4cc69498533eb978
sruthirameshv <srameshv@indiana.edu>
2016-09-26 17:15:08 -0400

    Added sudo permissions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,9 @@
 echo 'starting installation process' 
 cd '/home/ec2-user/DataIngestor/core/python/DataIngestor'
 
+sudo su
 pip install requests
 pip install Flask
 pip install boto
 export FLASK_APP=DataIngestor.py
-flask run --host=0.0.0.0 --port=35000 >> teambash.log 2>&1 &
+flask run --host=0.0.0.0 --port=35000 >> dataIngestor.log 2>&1 &
