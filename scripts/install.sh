d180b12f691054453f2fed805a87f3a6b0d70f45
sruthirameshv <srameshv@indiana.edu>
2016-09-27 00:57:03 -0400

    Changed rm -r

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,7 @@ yum -y install python34-pip
 python3 -m pip install django
 
 echo 'starting installation process'
+rm -r /home/ec2-user/ui
 mv /home/ec2-user/UI  /home/ec2-user/ui
 cd /home/ec2-user/ui/core/UI/SampleWeb
 sudo pip uninstall Django
