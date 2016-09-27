d93af2bed5f787a06e21b3b76452d25f68619b63
sruthirameshv <srameshv@indiana.edu>
2016-09-26 21:55:56 -0400

    Python files

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user/dataIngestor/core/python/DataIngestor
 
 export FLASK_APP=DataIngestor.py
 export FLASK_DEBUG=0
-screen -d -m flask run --host=0.0.0.0 --port=35000 >> dataIngestor.log 2>&1 &
+flask run --host=0.0.0.0 --port=35000
