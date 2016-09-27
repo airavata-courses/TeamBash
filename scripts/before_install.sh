6770347298eb2bb45faae00372a03d1da615e6d1
sruthirameshv <srameshv@indiana.edu>
2016-09-27 00:09:14 -0400

    Adding the UI

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,1 @@
 echo 'killing existing Django process if any'
-fuser -k 8080/tcp
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,4 +7,4 @@ mv /home/ec2-user/UI  /home/ec2-user/ui
 cd /home/ec2-user/ui/core/ui/
 
 python manage.py runserver 0.0.0.0:8080 > /dev/null 2> /dev/null < /dev/null &
-exit 0
+
