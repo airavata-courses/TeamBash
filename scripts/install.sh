a7408934c339ca5aad463325fecee335be8dd2f1
sruthirameshv <srameshv@indiana.edu>
2016-09-26 21:37:18 -0400

    Mani is the saviour

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,7 @@
 echo 'starting installation process'
 mv /home/ec2-user/DataIngestor  /home/ec2-user/dataIngestor
-cd '/home/ec2-user/dataIngestor/core/python/DataIngestor'
+cd /home/ec2-user/dataIngestor/core/python/DataIngestor
 
-flask
 export FLASK_APP=DataIngestor.py
 export FLASK_DEBUG=0
 screen -d -m flask run --host=0.0.0.0 --port=35000 >> dataIngestor.log 2>&1 &
