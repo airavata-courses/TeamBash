314215b19dcfbf4b2952838e057a6bf53d89f09f
rraavi <raghuveer.raavi@gmail.com>
2016-09-26 23:23:03 -0400

    #13 travis, appspec, script bug fixes

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,3 +1,4 @@
-echo 'Terminating existing flask processes ' 
-ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill
-sleep 5
+echo 'killing existing flask process if any'
+sudo touch /var/log/teambash.log
+
+ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/teambash.log 2>&1 &
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,8 @@
 echo 'starting installation process'
-cd '/home/ec2-user/stormDetection'
-
-echo 'Activating virtualenv for stormDetection Microservice'
-pip install virtualenv
-virtualenv venv
-. venv/bin/activate
-pip install Flask
-export FLASK_APP=stormDetection.py 
-nohup flask run --host=0.0.0.0 --port=34000 &
-
+mv /home/ec2-user/StormDetection  /home/ec2-user/stormDetection
+cd /home/ec2-user/stormDetection/core/python/
+chmod 777 stormDetection
+cd stormDetection
+export FLASK_APP=stormDetection.py
+export FLASK_DEBUG=0
+flask run --host=0.0.0.0 --port=34000 >> dataIngestor.log 2>&1 &
