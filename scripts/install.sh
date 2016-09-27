e51a67683a7e8828dfbc32331161098b2519c902
sruthirameshv <srameshv@indiana.edu>
2016-09-27 01:07:14 -0400

    Added rm -r

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 echo 'starting installation process'
+rm -r /home/ec2-user/dataIngestor
 mv /home/ec2-user/DataIngestor  /home/ec2-user/dataIngestor
 cd /home/ec2-user/dataIngestor/core/python/
 chmod 777 DataIngestor
