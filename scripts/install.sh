378b0ee0e0dcf8f005267261cbba7b1b7f38027d
rraavi <raghuveer.raavi@gmail.com>
2016-09-27 01:21:48 -0400

    rm command for install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 echo 'starting installation process'
+rm -r /home/ec2-user/stormDetection
 mv /home/ec2-user/StormDetection  /home/ec2-user/stormDetection
 cd /home/ec2-user/stormDetection/core/python/
 chmod 777 stormDetection
