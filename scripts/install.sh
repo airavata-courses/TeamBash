513f0032d29ed8385fb90dd49ec317d4571ba2e4
sruthirameshv <srameshv@indiana.edu>
2016-09-26 21:26:57 -0400

    Added the screen

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,9 +2,7 @@ echo 'starting installation process'
 mv /home/ec2-user/DataIngestor  /home/ec2-user/dataIngestor
 cd '/home/ec2-user/dataIngestor/core/python/DataIngestor'
 
-sudo su
-pip install requests
-pip install Flask
-pip install boto
+flask
 export FLASK_APP=DataIngestor.py
-flask run --host=0.0.0.0 --port=35000 >> dataIngestor.log 2>&1 &
+export FLASK_DEBUG=0
+screen -d -m lask run --host=0.0.0.0 --port=35000 >> dataIngestor.log 2>&1 &
