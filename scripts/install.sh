fba03dafbf34a89143d62195e271f982ea6fc9ca
sruthirameshv <srameshv@indiana.edu>
2016-09-27 00:27:11 -0400

    Adding the UI to diffrent port

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,8 @@ python3 -m pip install django
 echo 'starting installation process'
 mv /home/ec2-user/UI  /home/ec2-user/ui
 cd /home/ec2-user/ui/core/ui/
-
-python manage.py runserver 0.0.0.0:8080 > /dev/null 2> /dev/null < /dev/null &
+sudo pip uninstall Django
+sudo pip install Django==1.10.1
+sudo pip install httplib2
+python manage.py runserver 0.0.0.0:9001 > /dev/null 2> /dev/null < /dev/null &
 
