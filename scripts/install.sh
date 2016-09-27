7b58c1e043d77b2e6b22d937087948712b4a49f8
sruthirameshv <srameshv@indiana.edu>
2016-09-26 22:15:50 -0400

    Fixed

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,8 @@
 echo 'starting installation process'
 mv /home/ec2-user/DataIngestor  /home/ec2-user/dataIngestor
-cd /home/ec2-user/dataIngestor/core/python/DataIngestor
-chmod 777
+cd /home/ec2-user/dataIngestor/core/python/
+chmod 777 DataIngestor
+cd DataIngestor
 export FLASK_APP=DataIngestor.py
 export FLASK_DEBUG=0
 flask run --host=0.0.0.0 --port=35000
