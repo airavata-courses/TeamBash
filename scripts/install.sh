c9082e7b4c73561f957757530d49c7277129b293
Manikandan Murugesan <manikandan.m@live.co.uk>
2016-09-26 20:10:22 -0400

    Hopefully final push

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 echo 'Installing Registry' 
-cd "/home/ec2-user/registryService/core/java/registry"
+mv /home/ec2-user/registryService registry
+cd "/home/ec2-user/registry/core/java/registry"
 sudo mvn -e clean install
 java -jar target/registry-1.0.0.jar server registry.yml >> registry.log 2>&1 &
