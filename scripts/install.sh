68d953d777732777fc5ac54e0f9dcb0e7440ffab
Manikandan Murugesan <manikandan.m@live.co.uk>
2016-09-26 20:57:35 -0400

    Bug fix -1

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 echo 'Installing Registry' 
-mv /home/ec2-user/registryService registry
+mv /home/ec2-user/registryService /home/ec2-user/registry
 cd "/home/ec2-user/registry/core/java/registry"
 sudo mvn -e clean install
 java -jar target/registry-1.0.0.jar server registry.yml >> registry.log 2>&1 &
