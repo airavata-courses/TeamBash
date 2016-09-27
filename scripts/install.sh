bb248ec045d662e936f8a46738e090fdec6fd859
sruthirameshv <srameshv@indiana.edu>
2016-09-26 21:31:57 -0400

    Added the screen

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,4 +5,4 @@ cd '/home/ec2-user/dataIngestor/core/python/DataIngestor'
 flask
 export FLASK_APP=DataIngestor.py
 export FLASK_DEBUG=0
-screen -d -m lask run --host=0.0.0.0 --port=35000 >> dataIngestor.log 2>&1 &
+screen -d -m flask run --host=0.0.0.0 --port=35000 >> dataIngestor.log 2>&1 &
