829cecf9e5958faf32b4739fc5cbb6dfe7322cfd
janak1710 <janak1710@gmail.com>
2016-10-22 16:02:40 -0400

    Updating permissions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 echo 'starting installation process'
 rm -r /home/ec2-user/stormDetection
 mv /home/ec2-user/StormDetection  /home/ec2-user/stormDetection
+cd /home/ec2-user/stormDetection/
 chmod 777 stormDetection
 cd stormDetection
 echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
