542b4f1c4b3f1978e7026d239bfca9c5e835b1a3
Janak Bhalla <janak1710@gmail.com>
2016-10-22 13:33:30 -0400

    Updating install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,11 @@
+echo 'sstarting installation process'
+rm -r /home/ec2-user/dataIngestor
+mv /home/ec2-user/DataIngestor  /home/ec2-user/dataIngestor
+cd /home/ec2-user/dataIngestor/
+chmod 777 dataIngestor
+cd dataIngestor
+python DataIngestor.py  >> dataIngestor.log 2>&1 &
 echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
-docker build -t teambash/data-ingestor-service:v1 DataIngestor 2>&1
+docker build -t teambash/data-ingestor-service:v1 . >> /var/log/sga-docker.log 2>&1
 echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
 docker run -it --name data-ingestor-service -p 65000:65000 -d teambash/data-ingestor-service:v1
