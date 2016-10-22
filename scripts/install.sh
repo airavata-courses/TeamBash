c985b27f21b3e8d7974a8871f4936443e7c5c7d7
sruthirameshv <srameshv@indiana.edu>
2016-10-22 16:10:32 -0400

    Changed the script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,14 @@
-yum -y install python34
-yum -y install python34-pip
-python3 -m pip install django
+yum -y install python27
+yum -y install python2-pip
+python -m pip install django
+pip install django-social-auth
 
 echo 'starting installation process'
 rm -r /home/ec2-user/ui
 mv /home/ec2-user/UI  /home/ec2-user/ui
 cd /home/ec2-user/ui/core/UI/SampleWeb
 sudo pip uninstall Django
-sudo pip install Django==1.10.1
+sudo pip install Django==1.8.1
 sudo pip install httplib2
 python manage.py runserver 0.0.0.0:9001 >>DjangoUi.log > /dev/null 2> /dev/null < /dev/null &
 
