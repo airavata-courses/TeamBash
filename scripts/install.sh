ea5db240ba3769526603322392300f5dfb1eb891
Janak Bhalla <janak1710@gmail.com>
2016-10-22 13:21:15 -0400

    Removing log file

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
-docker build -t teambash/data-ingestor-service:v1 DataIngestor >> /var/log/sga-docker.log 2>&1
+docker build -t teambash/data-ingestor-service:v1 DataIngestor 2>&1
 echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
 docker run -it --name data-ingestor-service -p 65000:65000 -d teambash/data-ingestor-service:v1
