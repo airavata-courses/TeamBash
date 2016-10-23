9b82b9e408080f67bddb46d4ef576828d674c23c
Manikandan Murugesan <manikandan.m@live.co.uk>
2016-10-23 19:18:33 -0400

    Unit Test Setup - 2

---
+++ core/java/registry/src/main/java/iu/edu/teambash/resources/UserResourceTest.java
@@ -0,0 +1,16 @@
+package iu.edu.teambash.resources;
+
+import org.junit.Test;
+
+/**
+ * Created by murugesm on 10/23/16.
+ */
+public class UserResourceTest
+{
+    @Test
+    public void createUser() throws Exception
+    {
+
+    }
+
+}
