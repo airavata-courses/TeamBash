eda0d0e3ad97a27dee63f6c02d239cdde971fc8e
Manikandan Murugesan <manikandan.m@live.co.uk>
2016-10-23 19:17:57 -0400

    Unit Test Setup

---
+++ core/java/registry/src/main/java/iu/edu/teambash/resources/DisplayDataTest.java
@@ -0,0 +1,26 @@
+package iu.edu.teambash.resources;
+
+/**
+ * Created by murugesm on 10/23/16.
+ */
+public class DisplayDataTest
+{
+    @org.junit.Test
+    public void display() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void insertService() throws Exception
+    {
+
+    }
+
+    @org.junit.Test
+    public void endService() throws Exception
+    {
+
+    }
+
+}
