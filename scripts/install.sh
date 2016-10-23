94ff9b9c16cbb9248cd48f3331a771f44f1f6de6
Janak Bhalla <janak1710@gmail.com>
2016-10-22 22:55:51 -0400

    Remove mvn

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,10 @@
 echo 'Installing Registry' 
 rm -r /home/ec2-user/registry
 mv /home/ec2-user/registryService /home/ec2-user/registry
-cd "/home/ec2-user/registry/core/java/registry"
-sudo mvn -e clean install
 cd /home/ec2-user/registry/
 chmod 777 registry
 cd registry
 echo '===============Building docker===============' >> /var/log/sga-docker.log 2>&1
 docker build -t teambash/registry-service:v1 . >> /var/log/sga-docker.log 2>&1
 echo '===============Running docker===============' >> /var/log/sga-docker.log 2>&1
-docker run -it --name sregistry-service -p 7777:7777 -d teambash/registry-service:v1
+docker run -it --name registry-service -p 7777:7777 -d teambash/registry-service:v1
